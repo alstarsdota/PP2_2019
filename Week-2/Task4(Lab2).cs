1faf6d17657ce3cc571fbb77e565bf87caf8fd25
Koblan <47256452+alstarsdota@users.noreply.github.com>
2019-02-09 02:29:21 +0600

    Add files via upload

---
+++ Week-2/Task4(Lab2).cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace W3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\kobaaaa\path1.txt";    //paths for the creating files
+            string path2 = @"C:\kobaaaa\path2.txt";   
+            string text = "Some text";            
+            StreamWriter sw = new StreamWriter(path1); //for the writing to first file
+            sw.Write(text);                            //write the text to first file
+            sw.Close();                                 //close the text cause of streamwriter
+            File.Copy(path1, path2);                    //copy paste the text from path1 to path2
+            File.Delete(path1);                         //delete the path1
+        }
+    }
+}
---
+++ Week-2/task1(Lab2).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3
+{
+    class Program
+    {
+        static bool palindrome(string a)        //checks the ?is word palindrome?
+        {
+            bool t = true;
+            for (int i = 0; i < a.Length / 2; i++)
+            {
+               if(a[i]!= a[a.Length - 1-i])
+                { return false; }
+            }
+            return t; 
+            
+        }
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Program Files\Task1\test.txt");//reads the text in file in given path as one word
+            
+            if (palindrome(text))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            { Console.WriteLine("No"); }
+            Console.ReadKey();
+        }
+    }
+}
